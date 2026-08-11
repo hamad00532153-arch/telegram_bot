3b56d10f4df2b883faf1eb71372505a1bf3baaa8
hamad00532153-arch <hamad.00532153@gmail.com>
2026-08-11 13:25:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def run_web():
         use_reloader=False
     )
     def run_web():
-    port = int(os.environ.get("PORT", 10000))
+     port = int(os.environ.get("PORT", 10000))
 
     app.run(
         host="0.0.0.0",
